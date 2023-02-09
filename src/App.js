6ab7b8cd7289939c6577e0a608102d9c6a5782f9
trupti0406 <truptivijayyadav27@gmail.com>
2023-02-10 00:03:41 +0530

    slider remaining

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from "./components/Home";
 
 function App() {
   return (
+    
     <div className="App">
       <Home/>
     </div>
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./css/Home.css";
 
 const Slider = () => {
-  return <h1>Slider</h1>;
+  return (
+    <>
+      <h1>Slider</h1>
+    </>
+  );
 };
 
 export default Slider;
